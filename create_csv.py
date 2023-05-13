bf43c116ccae7f342f9970bb9d55e76025cd2730
alex <lxlx19@gmail.com>
2023-05-13 09:26:12 -0300

    Commit inicial

---
+++ create_csv.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+
+data = pd.DataFrame(
+    {
+        "icecream_flavor": np.random.choice(
+            [
+                "chocolate",
+                "vanilla",
+                "strawberry",
+                "pistachio",
+                "cookies and cream",
+                "mint chocolate chips",
+            ], 10000,
+        ),
+        "icecream_price": np.random.randint(100, 1000, 10000) / 100,
+        "icecream_rating": np.random.randint(1, 5, 10000),
+        "customer_id": np.random.choice(
+            [f"A{str(np.random.randint(1000, 9999))}" for _ in range(10)]
+            + [f"B{str(np.random.randint(1000, 9999))}" for _ in range(10)]
+            + [f"C{str(np.random.randint(1000, 9999))}" for _ in range(10)]
+            + [f"D{str(np.random.randint(1000, 9999))}" for _ in range(10)],
+            10000,
+        ),
+        "shop_city": np.random.choice(
+            [
+                "São Paulo",
+                "Campinas",
+                "Barueri",
+                "Osasco"
+            ], 10000,
+        ),
+    }
+)
+
+data.to_csv("icecream.csv", index=False)
+
