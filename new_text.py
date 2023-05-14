cea34153ecc1ebb77b978054b10f6f41a86c4da5
lxlx19@gmail.com <lxlx19@gmail.com>
2023-05-14 11:48:35 -0300

    First Run

---
+++ new_text.py
@@ -0,0 +1,20 @@
+import os
+
+from dotenv import load_dotenv, find_dotenv
+from langchain.llms import OpenAI
+
+load_dotenv(find_dotenv())
+
+llm = OpenAI(temperature=0)
+
+status = True
+
+while status:
+    text = input("\nDigite sua pergunta: ")
+
+    print(llm(text)) 
+
+    test = input("Continuar [s]sim [n]não: ")
+
+    status = True if test == 's' else False
+
